c7554f3f028ea3460544e21a1ce3da4d5c782004
fabian_sellemond <sellemondfabian@gmail.com>
2024-09-12 17:10:04 +0200

    add routes and home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {PageNotFoundComponent} from "./components/page-not-found.component";
+import {HomeComponent} from "./components/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {path: 'home', component: HomeComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
